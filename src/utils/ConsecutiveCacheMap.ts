acbbeac1501878ebe7490eacb4d8967c900b770c
0b5vr <0b5vr@0b5vr.com>
2022-03-11 01:51:54 +0900

    performance: a slight set nonsense

--- src/utils/ConsecutiveCacheMap.ts
+++ src/utils/ConsecutiveCacheMap.ts
@@ -1,20 +1,19 @@
 export class ConsecutiveCacheMap<TKey, TValue> {
+  private __setShouldDelete: Set<TKey>;
   private readonly __map: Map<TKey, TValue>;
-  private readonly __setUsed: Set<TKey>;
 
   public constructor() {
     this.__map = new Map();
-    this.__setUsed = new Set();
+    this.__setShouldDelete = new Set();
   }
 
   public get( key: TKey ): TValue | undefined {
-    this.__setUsed.add( key );
+    this.__setShouldDelete.delete( key );
     return this.__map.get( key );
   }
 
   public getOrCreate( key: TKey, create: () => TValue ): TValue {
-    this.__setUsed.add( key );
-
+    this.__setShouldDelete.delete( key );
     let value = this.__map.get( key );
     if ( value == null ) {
       value = create();
@@ -24,17 +23,17 @@ export class ConsecutiveCacheMap<TKey, TValue> {
   }
 
   public set( key: TKey, value: TValue ): void {
-    this.__setUsed.add( key );
+    this.__setShouldDelete.delete( key );
     this.__map.set( key, value );
   }
 
   public resetUsedSet(): void {
-    this.__setUsed.clear();
+    this.__setShouldDelete = new Set( this.__map.keys() );
   }
 
   public vaporize( onVaporize?: ( entry: [ key: TKey, value: TValue ] ) => void ): void {
     Array.from( this.__map.entries() ).forEach( ( [ key, value ] ) => {
-      if ( !this.__setUsed.has( key ) ) {
+      if ( this.__setShouldDelete.has( key ) ) {
         this.__map.delete( key );
         onVaporize?.( [ key, value ] );
       }
